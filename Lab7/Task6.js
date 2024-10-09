7153205fdb2f58c86b2710754f18293986c4ac08
AliRaza2006 <maliraza2019@gmail.com>
2024-10-09 13:07:57 +0500

    Add files via upload

---
+++ Lab7/Task6.js
@@ -0,0 +1,10 @@
+function sum(num1,num2)
+{
+    return num1+num2;
+}
+function length(string)
+{
+    return string.length;
+}
+console.log(sum(10,20));
+console.log(length("Hello World"))
---
+++ Lab7/Task7.js
@@ -0,0 +1,8 @@
+let car=
+{
+make: "Toyota",
+model: "Corolla",
+year: 2020,
+color: "Black"
+};
+console.log(car);
